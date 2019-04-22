e3b8ca7fe220588f0541af9d6ea8d256b5c90216
Chris-Humphrey <clhumphrey21@gmail.com>
2019-04-21 21:56:18 -0500

    upload list exercises

---
+++ list.py
@@ -0,0 +1,106 @@
+#======================#
+## List Exercises Part 1
+
+# numbers = [1,2,3,4,5]
+
+# print(sum(numbers,0))
+
+
+#======================#
+## List Exercises Part 2
+
+# num = [43,21,4,66,42,99]
+
+# print(max(num))
+
+
+#======================#
+## List Exercises Part 3
+
+# num = [43,21,4,66,42,99]
+
+# print(min(num))
+
+
+#======================#
+## List Exercises Part 4 - even numbers
+
+# num = [43,21,4,66,42,99]
+
+# for even in num:
+#     if (even % 2 == 0):
+#         print(even)
+
+
+#======================#
+## List Exercises Part 5 - positive numbers
+
+# num = [-20,5,10,-2,14]
+
+# for pos in num:
+#     if pos > 0:
+#         print(pos)
+
+
+#======================#
+## List Exercises Part 6 - positive numbers 2
+
+# num = [-20,5,10,-2,14]
+# newNum = []
+# for pos in num:
+#     if pos > 0:
+#         newNum.append(pos)
+
+# print(newNum)
+
+
+#======================#
+## List Exercises Part 7 - multiply list
+
+# num = [1,2,3,4,5]
+# factor = 3
+# newNum = []
+
+# for new in num:
+#     newNum.append(new * factor)
+
+# print(newNum)
+
+
+#======================#
+## List Exercises Part 8 - multiply vectors
+
+# num1 = [1,2,3,4,5]
+# num2 = [10,20,30,40,50]
+# newList = []
+# s2 = 0
+
+# for s1 in num1:
+#     newList.append(s1 * num2[s2])
+#     s2 +=1
+
+# print(newList)
+
+
+#======================#
+## List Exercises Part 9 - matrix addition
+#*** unfinished *** #
+
+# matrix1 = [[1,2],[3,4]]
+# matrix2 = [[5,6],[7,8]]
+# newMatrix = [[],[]]
+# s1 = len(matrix1)
+# s2 = len(matrix2)
+
+
+# newMatrix[0].append(matrix1[0][0] + matrix2[0][0])
+# newMatrix[0].append(matrix1[0][1] + matrix2[0][1])
+# newMatrix[1].append(matrix1[1][0] + matrix2[1][0])
+# newMatrix[1].append(matrix1[1][1] + matrix2[1][1])
+# print(newMatrix)
+
+
+# while True:
+#     if(s1 > 0)
+#     s1 -= 1
+
