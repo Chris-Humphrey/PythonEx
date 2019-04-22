2fad129f161b62511c227c4e401c4fbe99331859
Chris-Humphrey <clhumphrey21@gmail.com>
2019-04-21 21:52:50 -0500

    upload string exercise

---
+++ string.py
@@ -0,0 +1,62 @@
+## String Exercises Part 1
+# string = "chris humphrey"
+# print(string.upper())
+
+#======================#
+## String Exercises Part 2
+
+# string = "chris humphrey"
+# print(string.capitalize())
+
+#======================#
+## String Exercises Part 3
+
+# string = "chris humphrey"
+# print(string[::-1])
+
+#======================#
+## String Exercises Part 4
+# String is immutable
+
+# string = """
+# ALorem ipsum is placeholder text commonly used in the graphic, print, 
+# and publishing industries for previewing layouts and visual mockups.
+# """.upper()
+# print(string)
+
+# for letter in string:
+#     print(letter)
+#     newString = [""]
+#     if letter == "A":
+#         newString.insert("4")
+#     elif letter == "E":
+#         newString.insert("3")
+#     elif letter == "G":
+#         newString.insert("6")
+#     elif letter == "I":
+#         newString.insert("1")
+#     elif letter == "O":
+#         newString.insert("0")
+#     elif letter == "S":
+#         newString.insert("5")
+#     elif letter == "T":
+#         newString.insert("7")
+#     else:
+#         # newString.insert(letter)
+#         print(newString)
+
+#======================#
+## String Exercises Part 5
+# list.index(item)
+#*** UNFINISHED ***#
+
+# string = "cheese"
+# prevVowel = ""
+
+# for vowel in string:
+#     if(prevVowel == vowel):
+#         prevVowel = vowel
+#         print(vowel * 5)
+#     else:
+#         prevVowel = vowel
+#         print(vowel)
